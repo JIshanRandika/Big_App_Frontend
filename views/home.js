0058207a1b9e3bd3caaf5734ef603b75b3e92af9
JIshanRandika <jishanrandika@gmail.com>
2021-11-07 11:04:18 +0530

    dropdown update

--- views/home.js
+++ views/home.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import SearchableDropdown from '../item-name-dropdown/react-native-searchable-dropdown';
+import SearchableDropdown from '../item-name-dropdown';
 import {
     Platform, StyleSheet, View, Text,
     Image, TouchableOpacity, Alert, Dimensions, TextInput, KeyboardAvoidingView,ImageBackground
@@ -40,6 +40,8 @@ var items = [
         name: 'Swift',
     },
 ];
+
+
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
@@ -53,6 +55,9 @@ export default class Home extends React.Component {
                     id: 8,
                     name: 'Swift',
                 }
+            ],
+            selectedShops: [
+
             ]
         }
     }
@@ -67,6 +72,71 @@ export default class Home extends React.Component {
                         height: 200
                     }} source={require('../assets/images/logo.png')}/>
                 </View>
+
+
+                {/*shop dropdown=========================================================================*/}
+                <SearchableDropdown
+                    onItemSelect={(shop) => {
+                        console.log("testing")
+                        // this.setState({productSelected:false})
+
+                        const items = this.state.selectedShops;
+                        items.push(shop)
+                        this.setState({newselect:shop.name})
+                        this.setState({ selectedShops: items });
+
+                    }}
+                    containerStyle={{ padding: 5, width: "75%" }}
+                    onRemoveItem={(item, index) => {
+                        const items = this.state.selectedItems.filter((sitem) => sitem.id !== item.id);
+                        this.setState({ selectedShops: items });
+                    }}
+                    itemStyle={{
+                        padding: 10,
+                        marginTop: 2,
+                        backgroundColor: '#ddd',
+                        borderColor: '#bbb',
+                        borderWidth: 1,
+                        borderRadius: 5,
+                    }}
+                    itemTextStyle={{ color: '#222' }}
+                    itemsContainerStyle={{ maxHeight: 100 }}
+                    items={this.state.allProducts}
+                    // defaultIndex={2}
+                    resetValue={false}
+                    textInputProps={
+                        {
+                            placeholder: this.state.newselect,
+                            underlineColorAndroid: "transparent",
+                            style: {
+                                padding: 12,
+                                borderWidth: 1,
+                                borderColor: '#592828',
+                                borderRadius: 5,
+                            },
+                            // onTextChange: text => alert(text)
+                        }
+                    }
+                    listProps={
+                        {
+                            nestedScrollEnabled: true,
+                        }
+                    }
+                />
+
+
+
+
+
+
+
+
+
+
+
+
+
+                {/*item dropdown=========================================================================*/}
                 {/* Multi */}
                 <SearchableDropdown
                     multi={true}
