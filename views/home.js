de03d1b391851af4ef91a76b3ffb3a5762fe90be
JIshanRandika <jishanrandika@gmail.com>
2021-11-18 10:54:37 +0530

    shop dropdown update

--- views/home.js
+++ views/home.js
@@ -9,40 +9,40 @@ import {
 } from 'react-native';
 
 
-var items = [
-    {
-        id: "1",
-        itemName: 'JavaScript',
-    },
-    {
-        id: "200",
-        itemName: 'Java',
-    },
-    {
-        id: "50",
-        itemName: 'Ruby',
-    },
-    {
-        id: "10",
-        itemName: 'React Native',
-    },
-    {
-        id: "20",
-        itemName: 'PHP',
-    },
-    {
-        id: "2",
-        itemName: 'Python',
-    },
-    {
-        id: "1",
-        itemName: 'Go',
-    },
-    {
-        id: "8",
-        itemName: 'Swift',
-    },
-];
+// var items = [
+//     {
+//         id: "1",
+//         itemName: 'JavaScript',
+//     },
+//     {
+//         id: "200",
+//         itemName: 'Java',
+//     },
+//     {
+//         id: "50",
+//         itemName: 'Ruby',
+//     },
+//     {
+//         id: "10",
+//         itemName: 'React Native',
+//     },
+//     {
+//         id: "20",
+//         itemName: 'PHP',
+//     },
+//     {
+//         id: "2",
+//         itemName: 'Python',
+//     },
+//     {
+//         id: "1",
+//         itemName: 'Go',
+//     },
+//     {
+//         id: "8",
+//         itemName: 'Swift',
+//     },
+// ];
 
 
 export default class Home extends React.Component {
@@ -71,13 +71,13 @@ export default class Home extends React.Component {
     componentDidMount() {
         this.setState({isLoading: true});
 
-        fetch('https://healthyfoodssabra.herokuapp.com/api/products')
-        // fetch('http://localhost:8080/api/items')
+        // fetch('https://healthyfoodssabra.herokuapp.com/api/products')
+        fetch('http://192.168.8.100:8080/api/shops')
             .then(response => response.json())
             .then(data => this.setState({allShops: data}));
 
         // fetch('https://healthyfoodssabra.herokuapp.com/api/products')
-            fetch('http://192.168.8.101:8080/api/items')
+            fetch('http://192.168.8.100:8080/api/items')
             .then(response => response.json())
             .then(data => this.setState({allItems: data}));
 
@@ -103,65 +103,54 @@ export default class Home extends React.Component {
 
 
                 {/*shop dropdown=========================================================================*/}
-                {/*<ShopDropdown*/}
-                {/*    onItemSelect={(shop) => {*/}
-                {/*        console.log("testing")*/}
-                {/*        // this.setState({productSelected:false})*/}
+                <ShopDropdown
+                    onItemSelect={(shop) => {
+                        console.log("testing")
+                        // this.setState({productSelected:false})
 
-                {/*        const shops = this.state.selectedShops;*/}
-                {/*        items.push(shop)*/}
-                {/*        this.setState({ newSelect:shop.name})*/}
-                {/*        this.setState({ selectedShops: shops });*/}
+                        const shops = this.state.selectedShops;
+                        items.push(shop)
+                        this.setState({ newSelect:shop.username})
+                        this.setState({ selectedShops: shops });
 
-                {/*    }}*/}
-                {/*    containerStyle={{ padding: 5, width: "75%" }}*/}
-                {/*    onRemoveItem={(shop, index) => {*/}
-                {/*        const shops = this.state.selectedItems.filter((sitem) => sitem.id !== shop.id);*/}
-                {/*        this.setState({ selectedShops: shops });*/}
-                {/*    }}*/}
-                {/*    itemStyle={{*/}
-                {/*        padding: 10,*/}
-                {/*        marginTop: 2,*/}
-                {/*        backgroundColor: '#ddd',*/}
-                {/*        borderColor: '#bbb',*/}
-                {/*        borderWidth: 1,*/}
-                {/*        borderRadius: 5,*/}
-                {/*    }}*/}
-                {/*    itemTextStyle={{ color: '#222' }}*/}
-                {/*    itemsContainerStyle={{ maxHeight: 100 }}*/}
-                {/*    items={this.state.allShops}*/}
-                {/*    // defaultIndex={2}*/}
-                {/*    resetValue={false}*/}
-                {/*    textInputProps={*/}
-                {/*        {*/}
-                {/*            placeholder: this.state.newSelect,*/}
-                {/*            underlineColorAndroid: "transparent",*/}
-                {/*            style: {*/}
-                {/*                padding: 12,*/}
-                {/*                borderWidth: 1,*/}
-                {/*                borderColor: '#592828',*/}
-                {/*                borderRadius: 5,*/}
-                {/*            },*/}
-                {/*            // onTextChange: text => alert(text)*/}
-                {/*        }*/}
-                {/*    }*/}
-                {/*    listProps={*/}
-                {/*        {*/}
-                {/*            nestedScrollEnabled: true,*/}
-                {/*        }*/}
-                {/*    }*/}
-                {/*/>*/}
-
-
-
-
-
-
-
-
-
-
-
+                    }}
+                    containerStyle={{ padding: 5, width: "75%" }}
+                    onRemoveItem={(shop, index) => {
+                        const shops = this.state.selectedItems.filter((sitem) => sitem.id !== shop.id);
+                        this.setState({ selectedShops: shops });
+                    }}
+                    itemStyle={{
+                        padding: 10,
+                        marginTop: 2,
+                        backgroundColor: '#ddd',
+                        borderColor: '#bbb',
+                        borderWidth: 1,
+                        borderRadius: 5,
+                    }}
+                    itemTextStyle={{ color: '#222' }}
+                    itemsContainerStyle={{ maxHeight: 100 }}
+                    items={this.state.allShops}
+                    // defaultIndex={2}
+                    resetValue={false}
+                    textInputProps={
+                        {
+                            placeholder: this.state.newSelect,
+                            underlineColorAndroid: "transparent",
+                            style: {
+                                padding: 12,
+                                borderWidth: 1,
+                                borderColor: '#592828',
+                                borderRadius: 5,
+                            },
+                            // onTextChange: text => alert(text)
+                        }
+                    }
+                    listProps={
+                        {
+                            nestedScrollEnabled: true,
+                        }
+                    }
+                />
 
 
                 {/*item dropdown=========================================================================*/}
@@ -199,7 +188,7 @@ export default class Home extends React.Component {
                     resetValue={false}
                     textInputProps={
                         {
-                            placeholder: "placeholder",
+                            placeholder: "Select your items",
                             underlineColorAndroid: "transparent",
                             style: {
                                 padding: 12,
